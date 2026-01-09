3a69188993fd4fa4c72a926d47352dfeca71c38a
theo <tjfer725@learner.dvusd.org>
2026-01-09 13:05:35 -0700

    started assignment

---
+++ pyfest/pyfest/pyfest.py
@@ -0,0 +1,17 @@
+# The initial lineup
+lineup = [
+    ("Ken Karson", "Rap", 67),
+    ("Yeat", "Rap", 41),
+    ("Playboi Carti", "Rap", 61)
+]
+
+# 1. Add the headliner
+headliner = ("Esdeekid", "UK Rap", 93)
+lineup.append(headliner)
+print(lineup)
+removed_band = lineup.pop(0)
+print(lineup)
+lineup.append(removed_band)
+print(lineup)
+
+# Continue the logic below...
