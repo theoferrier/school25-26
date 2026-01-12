45bc4c55d39cc8e749a6e4fc8500b9ee7225bda3
theo <tjfer725@learner.dvusd.org>
2026-01-12 13:04:14 -0700

    esdee

--- pyfest/pyfest/pyfest.py
+++ pyfest/pyfest/pyfest.py
@@ -8,10 +8,22 @@ lineup = [
 # 1. Add the headliner
 headliner = ("Esdeekid", "UK Rap", 93)
 lineup.append(headliner)
-print(lineup)
-removed_band = lineup.pop(0)
-print(lineup)
-lineup.append(removed_band)
-print(lineup)
 
-# Continue the logic below...
+print("\n--- Py-Fest 2026 Stage Manager ---")
+print("   1. View Lineup & Total Time")
+print("        2. Add a New Band")
+print("    3. Move First Band to End")
+print("     4. Remove Band by Name")
+print("   5. Move Band by Specific Name")
+print("            6. Exit")
+option = int(input("Select an option (1-6): "))
+while option != 6:
+    if option = 1:
+        print("\n--- Current Lineup ---")
+        total_time = 0
+    if option = 2:
+        new_band = str(input("Enter the band's name: "))
+        new_genre = str(input("Enter the band's genre: "))
+        new_playtime = int(input("Enter the band's play time"))
+    if option = 3:
+        late_band = lineup.pop(0)
