a7db312bc620ecb3440cbf814bc83a8c8d9099ca
theo <tjfer725@learner.dvusd.org>
2026-03-25 12:53:02 -0700

    update

--- blocks/blocks/blocks.py
+++ blocks/blocks/blocks.py
@@ -2,10 +2,13 @@ import pygame
 import random
 
 # Initialize Pygame
-pygame.init()
+pygame.init()aaaa
 
 # Constants
 WIDTH, HEIGHT = 800, 600
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
